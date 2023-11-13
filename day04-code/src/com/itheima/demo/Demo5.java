114e79781553985925afc5d0602d3681abf711f3
XiaoPaiM <122215146+XiaoPaiM@users.noreply.github.com>
2023-11-13 18:00:53 +0800

    Delete day04-code/src/com/itheima/demo directory

--- day04-code/src/com/itheima/demo/Demo5.java
+++
@@ -1,9 +0,0 @@
-package com.heima.demo;
-
-public class Demo5 {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 1000000000; i++) {
-            System.out.println("我爱你，李佳乐！");
-        }
-    }
-}
